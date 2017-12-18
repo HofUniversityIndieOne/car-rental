381b15d6de45fea647c4deba333f8f4abc9448a0
Oliver Hader <oliver@typo3.org>
2017-12-18 23:15:42 +0100

    [TASK] Finalize JsonView configuration

--- Classes/Controller/JsonController.php
+++ Classes/Controller/JsonController.php
@@ -1,15 +1,8 @@
 <?php
 namespace HofUniversityIndie\CarRental\Controller;
 
-use HofUniversityIndie\CarRental\Domain\Model\Car;
-use HofUniversityIndie\CarRental\Domain\Model\Customer;
-use HofUniversityIndie\CarRental\Domain\Model\Rental;
 use HofUniversityIndie\CarRental\Domain\Repository\CarRepository;
-use HofUniversityIndie\CarRental\Domain\Repository\RentalRepository;
-use HofUniversityIndie\CarRental\Service\Customer\InvalidSessionException;
-use HofUniversityIndie\CarRental\Service\Customer\SessionService;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Extbase\Property\TypeConverter\DateTimeConverter;
 
 /***
  * This file is part of the "Car Rental" Extension for TYPO3 CMS.
@@ -40,7 +33,24 @@ class JsonController extends ActionController
         $cars = $this->carRepository->findAll();
         /** @var \TYPO3\CMS\Extbase\Mvc\View\JsonView $view */
         $view = $this->view;
-        $view->assign('value', $cars);
-        $view->setConfiguration([]);
+        $view->assign('cars', $cars->toArray());
+        $view->setVariablesToRender(['cars']);
+        $view->setConfiguration([
+            'cars' => [
+                '_descendAll' => [
+                    '_exclude' => ['pid'],
+                    '_descend' => [
+                        'brand' => [
+                            '_exclude' => ['pid'],
+                        ],
+                        'tires' => [
+                            '_descendAll' => [
+                                '_exclude' => ['pid'],
+                            ],
+                        ]
+                    ]
+                ]
+            ],
+        ]);
     }
 }
